3b8ae28d91079196218f47ae389c091aaa5131d8
flxhm <fe.heim@gmx.de>
2020-11-25 22:38:08 +0100

    Claim2 update

--- Claim 02_Lockdown_measures/Lockdown_measures.R
+++ Claim 02_Lockdown_measures/Lockdown_measures.R
@@ -3,20 +3,52 @@ library(data.table)
 library(ggplot2)
 
 
-# importing Case, PatientInfo, Time
-case <- fread("./extData/Policy.csv")
+# import files: Case, PatientInfo, Time
+policy <- fread("./extData/Policy.csv")
 patientinfo <- fread("./extData/PatientInfo.csv")
 time <- fread("./extData/Time.csv")
 
+#-----------------------inspect data-------------------------------------------
 
-#patient info 
-nb_infectionCases <- patientinfo[, .N, by = "infection_case"]
+#total number of infection cases due to immigration
+contact_overseas <- patient_Info[infection_case == "overseas inflow", 
+                                 .N, by = "confirmed_date"]
+contact_overseas[1:10]
 
-setnames(nb_infectionCases, "N", "sum")
+#get subsets only for 3 different immigration policies 
+#IP_Special, IP_14Day, IP_ManTest
+immigration_policy <- policy[type == "Immigration"]
+
+IP_Special <- immigration_policy[gov_policy == "Special Immigration Procedure"]
+IP_14Day <- immigration_policy[gov_policy == "Mandatory 14-day Self-Quarantine"]
+IP_ManTest <- immigration_policy[gov_policy == "Mandatory Self-Quarantine & Diagonostic Tests"]
+
+#plot nb infection cases of immigration per day + start date of immigration policies
+
+ggplot(contact_overseas, aes(x=confirmed_date, y=N)) + geom_line() +
+  geom_vline(data = IP_Special, aes(xintercept = start_date, color = "Immigration\nprocedure"), 
+             linetype = "longdash", show.legend = TRUE)+
+  geom_vline(data = IP_14Day, aes(xintercept = start_date, color = "Self quarantine"), 
+             linetype = "longdash", show.legend = TRUE)+
+  geom_vline(data = IP_ManTest, aes(xintercept = start_date, color = "Diagnostic Test\nUS"), 
+             linetype = "longdash", show.legend = TRUE) +
+  labs(x = "confirmed date", y = "nb. infection cases (immigration)")
 
 
-#plot histogram 
-ggplot(nb_infectionCases[1:10], aes(x = sum , y = reorder(infection_case,sum))) + 
-  geom_bar(stat= "identity", width = .9) 
+#Germany
+patientinfo2 <- patientinfo[, .N, by = confirmed_date]
+patientinfo2[1:100]
 
-nb_infectionCases
+RKI_data <- fread("./extData/DE_InfectionCases.csv")
+RKI_data1 <- RKI_data[,c("Berichtsdatum", "Anzahl COVID-19-Fälle")]
+
+RKI_data1 <- RKI_data1[, confirmed_date := as.Date(Berichtsdatum, format ="%d.%m.%y")]
+
+comp_Germany <- merge(RKI_data1, patientinfo2, by = "confirmed_date", all = FALSE)
+
+
+
+
+
+
+
