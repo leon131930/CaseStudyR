bce6dac008b8a655cec149c8e0a2059a01a34c9b
ge96fum <leon.gaertner@tum.de>
2020-11-26 23:04:00 +0100

    .

--- Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
+++ Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
@@ -12,13 +12,12 @@ case <- fread("./extData/Case.csv")
 #subsetting to remove empty values
 case <- case[longitude != "-"]
 
-# change data type to integer
+# change data type to double
 case[, latitude := as.double(latitude)][, longitude := as.double(longitude)]
 
 
-
-# plot
+# plot coordinates with cases, different size
 ggplot(case, aes(x=latitude, y=longitude)) +
   geom_point(aes(size=confirmed))
 
-  
+#use scale_y_continuous() to display coordinates better??
