425d379dfac781eae1b6745a6bd61be60967dbd9
ge96fum <leon.gaertner@tum.de>
2020-11-26 22:00:59 +0100

    plot cases on map

--- Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
+++ Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
@@ -1,1 +1,21 @@
 ### Region visualization ###
+
+library(magrittr)
+library(data.table)
+library(ggplot2)
+library(ggmap)
+
+# importing Case
+case <- fread("./extData/Case.csv")
+
+#subsetting to remove empty values
+case <- case[longitude != "-"]
+
+# change data type to integer
+case[, latitude := as.double(latitude)][, longitude := as.double(longitude)]
+
+
+
+# plot
+ggplot(case, aes(x=latitude, y=longitude)) +
+  geom_point(aes(size=confirmed))       
