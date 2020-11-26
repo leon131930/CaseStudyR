029c96187c5491e04a094339af15240a80b25c89
ge96fum <leon.gaertner@tum.de>
2020-11-26 21:10:41 +0100

    just formatting

--- Claim 03_Region_and_Superspreader/First_Try_Superspreader.R
+++ Claim 03_Region_and_Superspreader/First_Try_Superspreader.R
@@ -19,13 +19,24 @@ patientinfo[, daily_cases := .N, by = confirmed_date]
 patientinfo
 
 # Creating a graph showing the Daily Cases over time
-ggplot(data = patientinfo, aes(x=confirmed_date, y=daily_cases)) + geom_line(colour="orange") + labs(x="Time", y="Cases per Day")
-ggplot(data = patientinfo, aes(x=confirmed_date, y=daily_cases)) + geom_line(colour="orange") + labs(x="Time", y="Cases per Day") + geom_hline(yintercept=100, color="green")
+ggplot(data = patientinfo, aes(x=confirmed_date, y=daily_cases)) +
+  geom_line(colour="orange") + labs(x="Time", y="Cases per Day")
+
+ggplot(data = patientinfo, aes(x=confirmed_date, y=daily_cases)) +
+  geom_line(colour="orange") + labs(x="Time", y="Cases per Day") +
+  geom_hline(yintercept=100, color="green")
 
 # Do we have some "Super-spreader" people within our data set?
 patientinfo %>% count(infected_by) %>% top_n(15, n) %>% arrange(desc(n))
 
 # Adding the 5 big Spreader Events to our Graph showing Daily Cases over Time
 ggplot(data = patientinfo, aes(x=confirmed_date, y=daily_cases)) +
-  geom_line(colour="grey") + geom_line(data = subset(patientinfo, infection_case=="Itaewon Clubs"), colour="green") + geom_line(data = subset(patientinfo, infection_case=="Richway"), colour="blue") + geom_line(data = subset(patientinfo, infection_case=="Guro-gu Call Center"), colour="purple") + geom_line(data = subset(patientinfo, infection_case=="Shincheonji Church"), colour="red") + geom_line(data = subset(patientinfo, infection_case=="Coupang Logistics Center"), colour="black") + labs(x="Time", y="Cases per Day") + geom_hline(yintercept=100, color="orange")
+  geom_line(colour="grey") +
+  geom_line(data = subset(patientinfo, infection_case=="Itaewon Clubs"), colour="green") +
+  geom_line(data = subset(patientinfo, infection_case=="Richway"), colour="blue") +
+  geom_line(data = subset(patientinfo, infection_case=="Guro-gu Call Center"), colour="purple") +
+  geom_line(data = subset(patientinfo, infection_case=="Shincheonji Church"), colour="red") +
+  geom_line(data = subset(patientinfo, infection_case=="Coupang Logistics Center"), colour="black") +
+  labs(x="Time", y="Cases per Day") +
+  geom_hline(yintercept=100, color="orange")
 ## suuuuuper long line of code - according to Google much easier when melting data beforehand - to be checked out 
