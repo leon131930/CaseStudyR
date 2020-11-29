6d6556be83180205221fb58a79b4ab7442e2d65c
flxhm <fe.heim@gmx.de>
2020-11-29 18:40:13 +0100

    Claim2: Fehlende Dateien hochgeladen

---
+++ Claim 02_Lockdown_measures/Lockdown_administrative.R
@@ -0,0 +1,21 @@
+library(magrittr)
+library(data.table)
+library(ggplot2)
+
+
+# import files: Time, policy
+time <- fread("./extData/Time.csv")
+policy <- fread("./extData/Policy.csv")
+
+#total cases in Korea per day
+time[,confirmed_date:=as.Date(date,"%d/%m/%y")]
+
+
+
+
+administration_plot <- ggplot(time, aes(x=confirmed_date, y=daily_cases)) + geom_line() +
+  geom_rect(aes(xmin=as.Date('2020-05-08'),xmax=as.Date('2020-06-07'),ymin=110,ymax=113,color="Close bars\nand clubs"))+
+  geom_rect(aes(xmin=as.Date('2020-05-16'),xmax=as.Date('2020-06-30'),ymin=120,ymax=123,color="Local government\nadministrative orders"))+
+  geom_rect(aes(xmin=as.Date('2020-05-21'),xmax=as.Date('2020-06-03'),ymin=130,ymax=133,color="Close karaoke"))+
+  labs(x = "confirmed date", y = "daily cases")+
+  theme(legend.position="bottom")
