57441536ea9de84c30b4af3f412f486eb72f2728
Jana <Jana@Janas-Air-2.fritz.box>
2020-11-29 12:22:17 +0100

    changing longitude and latitude, adding a map of South Korea

--- Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
+++ Claim 03_Region_and_Superspreader/Region_cases_vizualization.R
@@ -23,7 +23,37 @@ ggplot(case, aes(x=latitude, y=longitude)) +
   scale_y_continuous(labels = scales::comma)+  # to show comma numbers
   labs(title = "Confirmed Covid-cases mapped")
 
+# Doing plot again with y as latitude and x as longitude
+ggplot(case, aes(x=longitude, y=latitude)) +
+  geom_point(aes(size=confirmed, col=province)) +
+  scale_x_continuous(labels = scales::comma)+  #to show comma numbers
+  scale_y_continuous(labels = scales::comma)+  # to show comma numbers
+  labs(title = "Confirmed Covid-cases mapped")
+
 # to add here: Map of SouthKorea -> Library to import Map Data? -> Maybe just use Region table for coordinates of cities, and display them on top?
 # Possible to increase dot size?
 
+# Loading some additional packages that we might need
+library("ggplot2")
+theme_set(theme_bw())
+library("sf")
+library("rnaturalearth")
+library("rnaturalearthdata")
+
+# Displaying world map with ggplot2
+ggplot(data = world) +
+  geom_sf() +
+  xlab("Longitude") + ylab("Latitude") +
+  ggtitle("World map")
+# How can we zoom this in on South Korea in a way that it matches the dots we created above?
+# Trying an example from the gulf of Mexico
+ggplot(data = world) +
+  geom_sf() +
+  coord_sf(xlim = c(-102.15, -74.12), ylim = c(7.65, 33.97), expand = FALSE)
+
+# Now transferring this to South Korea - exact longitude and latidude coordinates to be found
+ggplot(data = world) +
+  geom_sf() +
+  coord_sf(xlim = c(125.14, 130.15), ylim = c(33.02, 38.98), expand = FALSE)
+# To be continued --> we still have to look up how we add the layer with our dots from above
 
