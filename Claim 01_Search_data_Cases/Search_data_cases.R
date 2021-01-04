e59f7ab685973355454b18a5c76b33917eda226d
ge96fum <leon.gaertner@tum.de>
2021-01-04 22:48:42 +0100

    Some graph changes, started with statistical testing

--- Claim 01_Search_data_Cases/Search_data_cases.R
+++ Claim 01_Search_data_Cases/Search_data_cases.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(dplyr)
 library(patchwork)
 library(hrbrthemes)
+library(ggpubr)
 
 
 # importing Case, PatientInfo, Searchtrend
@@ -13,7 +14,6 @@ case <- fread("./extData/Case.csv")
 patientinfo <- fread("./extData/PatientInfo.csv")
 searchtrend <- fread("./extData/SearchTrend.csv")
 
-case
 #create new column for number of daily cases, based on count of confirmed_date
 patientinfo <- patientinfo[, daily_cases := .N, by = confirmed_date]
 
@@ -111,23 +111,56 @@ merged_dt <- merge(time, searchtrend, by = "date", all = TRUE)
 
 #plot with 2 axis
 ggplot(merged_dt[date %between% c("2020-02-15", "2020-06-15")],aes(x=date)) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b %y")+
+  scale_x_date(date_breaks = "1 week", date_labels = "%W")+
   geom_line(aes(y=daily_cases, col = "Daily Cases")) +
-  geom_line(aes(y=(cold+flu+coronavirus)*8, col = "Searches for 'coronavirus'")) +
-  labs(title="Daily covid cases & search volume for 'coronavirus'")+
+  geom_line(aes(y=(coronavirus)*8, col = "Searches for 'coronavirus'")) +
+  labs(x="Calendar week 2020", title="Daily covid cases & relative search volume for 'coronavirus'")+
   scale_y_continuous(
-    # Features of the first axis
     name = "daily confirmed cases",
-    # Add a second axis and specify its features
-    sec.axis = sec_axis(trans=~./8*10000, name="Search Volume",labels = scales::comma))+
+    sec.axis = sec_axis(trans=~./8, name="relative search volume",labels = scales::comma))+
   theme(legend.position="bottom", legend.title = element_blank(), 
         axis.title.y.right = element_text(angle=90, color = searchcolour),
         axis.title.y.left = element_text(color = dailycasecolour))
 
+#same plot, but with search volume on left y axis
+ggplot(merged_dt[date %between% c("2020-02-15", "2020-06-29")],aes(x=date)) +
+ # scale_x_date(date_breaks = "1 week", date_labels = "%W")+
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %y")+
+  geom_line(aes(y=(coronavirus), col = "relative search volume")) +
+  geom_line(aes(y=daily_cases/8, col = "daily confirmed cases")) +
+  labs(x="Calendar week 2020", title="Daily covid cases & relative search volume for 'coronavirus'")+
+  scale_y_continuous(
+    name = "relative search volume",
+    sec.axis = sec_axis(trans=~.*8, name="daily confirmed cases",labels = scales::comma))+
+  theme(legend.position="bottom", legend.title = element_blank(), 
+        axis.title.y.right = element_text(angle=90, color = dailycasecolour),
+        axis.title.y.left = element_text(color = searchcolour))
+
+#explore maximum search volume data points
+#merged_dt[date %between% c("2019-12-01", "2020-05-01")][order(-coronavirus)]
+
+#calculate correlation for date range Mid February - End of June
+merged_dt1 <- merged_dt[date %between% c("2020-02-15", "2020-06-29")]
+corr <- cor(merged_dt1$daily_cases, merged_dt1$coronavirus, method = 'pearson')
+corr
 
 
-ggplot(merged_dt[date %between% c("2019-12-01", "2020-05-01")],aes(x=date)) +
-  geom_line(aes(y=coronavirus))
-                 
-merged_dt[date %between% c("2019-12-01", "2020-05-01"), max(coronavirus)]
+# Test: Correlation search volume and daily cases
+# Nullhypothesis: No Correlation
+ho_0 <- 0
+# Na's in data?
+anyNA(merged_dt1$daily_cases, merged_dt1$coronavirus)
+n <- nrow(merged_dt1)
 
+
+####################Just copied from weather claim - NOT YET CLEAR WHAT TO DO HERE################
+#test statistic
+t <- corr/ sqrt(1-corr^2)*sqrt(n-2)
+t
+# kritical value
+alpha <- 0.05
+z_krit <- qnorm(1-alpha/2)
+# Lehne H0 ab, wenn der Absolutwert (Betrag) der Teststatistik größer ist als der kritische Wert:
+abs(t) > z_krit
+
+          
