e0e70b7f58db8dc0f61d7a29b279fab709dc158f
ja97na <ja97na@web.de>
2021-01-04 18:26:11 +0100

    Claim weather

--- Claim 4_Weather/Weather_Plot.R
+++ Claim 4_Weather/Weather_Plot.R
@@ -21,11 +21,22 @@ timeProvince[, date:= as.IDate(date)]
 melted_dt <- merge(weather,timeProvince,by=c('province','date'))
 
 # Plot data: avg weather
-ggplot(melted_dt,aes(avg_temp,daily_cases))+geom_point()
+ggplot(melted_dt,aes(avg_temp,daily_cases,color=province))+geom_point()
 
 #Plot data: avg relative humidity
-ggplot(melted_dt,aes(avg_relative_humidity,daily_cases))+geom_point()
+ggplot(melted_dt,aes(avg_relative_humidity,daily_cases,color=province))+geom_point()
 
 # Correlation
 cor_temp <- cor(melted_dt$avg_temp, melted_dt$daily_cases, method = 'pearson')
 cor_humidity <- cor(melted_dt$avg_relative_humidity, melted_dt$daily_cases, method = 'pearson')
+
+
+# Plot data without province Daegu (Shincheonji church is located there -> Superspreader Event)
+
+
+
+# Province Busan
+melted_dt_Busan <- melted_dt[province=="Busan"]
+ggplot(melted_dt_Busan,aes(avg_temp,daily_cases))+geom_point()
+cor(melted_dt_Busan$avg_temp, melted_dt_Busan$daily_cases, method = 'pearson')
+cor(melted_dt_Busan$avg_relative_humidity, melted_dt_Busan$daily_cases, method = 'pearson')
