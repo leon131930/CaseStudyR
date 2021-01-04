529d2816abc4224c6c8d13aef73af8967b4989a4
ja97na <ja97na@web.de>
2021-01-04 19:46:43 +0100

    Claim 4

--- Claim 4_Weather/Weather_Plot.R
+++ Claim 4_Weather/Weather_Plot.R
@@ -31,12 +31,13 @@ cor_temp <- cor(melted_dt$avg_temp, melted_dt$daily_cases, method = 'pearson')
 cor_humidity <- cor(melted_dt$avg_relative_humidity, melted_dt$daily_cases, method = 'pearson')
 
 
-# Plot data without province Daegu (Shincheonji church is located there -> Superspreader Event)
-
-
-
-# Province Busan
-melted_dt_Busan <- melted_dt[province=="Busan"]
-ggplot(melted_dt_Busan,aes(avg_temp,daily_cases))+geom_point()
-cor(melted_dt_Busan$avg_temp, melted_dt_Busan$daily_cases, method = 'pearson')
-cor(melted_dt_Busan$avg_relative_humidity, melted_dt_Busan$daily_cases, method = 'pearson')
+# Data without province Daegu (Shincheonji church is located there -> Superspreader Event)
+# Prepare data
+melted_dt_without_Daegu = melted_dt[province!='Daegu']
+# Plot data: avg weather
+ggplot(melted_dt_without_Daegu,aes(avg_relative_humidity,daily_cases,color=province))+geom_point()
+# Plot data: avg relative humidity
+ggplot(melted_dt_without_Daegu,aes(avg_relative_humidity,daily_cases,color=province))+geom_point()
+# Correlation
+cor_temp_without_Daegu <- cor(melted_dt_without_Daegu$avg_temp, melted_dt_without_Daegu$daily_cases, method = 'pearson')
+cor_humidity_without_Daegu <- cor(melted_dt_without_Daegu$avg_relative_humidity, melted_dt_without_Daegu$daily_cases, method = 'pearson')
