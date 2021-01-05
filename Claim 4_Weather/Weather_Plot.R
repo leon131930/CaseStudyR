2c19452480e4a85d531dc27a4c8392b588372ad6
ja97na <ja97na@web.de>
2021-01-05 16:19:26 +0100

    Weather Plot

--- Claim 4_Weather/Weather_Plot.R
+++ Claim 4_Weather/Weather_Plot.R
@@ -30,7 +30,9 @@ ggplot(melted_dt,aes(avg_relative_humidity,daily_cases,color=province))+geom_poi
 cor_temp <- cor(melted_dt$avg_temp, melted_dt$daily_cases, method = 'pearson')
 cor_humidity <- cor(melted_dt$avg_relative_humidity, melted_dt$daily_cases, method = 'pearson')
 
-# Test: Correlation temperature and daily cases
+cor_temp_spear <- cor(melted_dt$avg_temp, melted_dt$daily_cases, method = 'spearman')
+
+# PearsonTest: Correlation temperature and daily cases
 # Nullhypothese: Keine Korrelation
 ho_0 <- 0
 # Na's in Daten?
@@ -45,6 +47,27 @@ z_krit <- qnorm(1-alpha/2)
 abs(t) > z_krit
 
 
+# Spearman Test: Correlation temperature and daily cases
+# Nullhypothese: Keine Korrelation
+ho_0 <- 0
+# Na's in Daten?
+anyNA(melted_dt$avg_temp, melted_dt$daily_cases)
+n <- nrow(melted_dt)
+# Teststatistik
+t_spear <- cor_temp_spear/ sqrt(1-cor_temp_spear^2)*sqrt(n-2)
+# kritischer Wert
+alpha <- 0.05
+z_krit <- qt(p=0.95,df=n-1)
+# Lehne H0 ab, wenn der Absolutwert (Betrag) der Teststatistik größer ist als der kritische Wert:
+abs(t_spear) > z_krit
+
+
+
+
+
+
+
+
 
 
 
@@ -52,9 +75,9 @@ abs(t) > z_krit
 # Prepare data
 melted_dt_without_Daegu = melted_dt[province!='Daegu']
 # Plot data: avg weather
-ggplot(melted_dt_without_Daegu,aes(avg_relative_humidity,daily_cases,color=province))+geom_point()
+ggplot(melted_dt_without_Daegu,aes(avg_temp,daily_cases,color=province))+geom_point()
 # Plot data: avg relative humidity
 ggplot(melted_dt_without_Daegu,aes(avg_relative_humidity,daily_cases,color=province))+geom_point()
 # Correlation
-cor_temp_without_Daegu <- cor(melted_dt_without_Daegu$avg_temp, melted_dt_without_Daegu$daily_cases, method = 'pearson')
+cor_temp_without_Daegu <- cor(melted_dt_without_Daegu$avg_temp, melted_dt_without_Daegu$daily_cases, method = 'spearman')
 cor_humidity_without_Daegu <- cor(melted_dt_without_Daegu$avg_relative_humidity, melted_dt_without_Daegu$daily_cases, method = 'pearson')
